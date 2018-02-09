5a5d602fdc7fb8e0e6e179ecbed78bb6d888c48b
PuppyYuan <381032913@qq.com>
2018-02-09 23:47:00 +0800

    initial commit

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    private bool isFly = false;
+    private bool isReach = false;
+    public float speed = 15;
+
+    private Transform startPoint;
+    private Transform circle;
+    private Vector3 targetCirclePos;
+
+    void Start()
+    {
+        startPoint = GameObject.Find("StartPoint").transform;
+        circle = GameObject.FindGameObjectWithTag("Circle").transform;
+        targetCirclePos = circle.position;
+        targetCirclePos.y -= 1.55f;
+    }
+
+    void Update()
+    {
+        if (!isFly)
+        {
+            if (!isReach)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, startPoint.position) < 0.05f)
+                {
+                    isReach = true;
+                }
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetCirclePos, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, targetCirclePos) < 0.05f)
+            {
+                transform.position = targetCirclePos;
+                transform.parent = circle;
+                isFly = false;
+            }
+        }
+
+    }
+
+    public void StartFly()
+    {
+        isFly = true;
+        isReach = true;
+    }
+}
